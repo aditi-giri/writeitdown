1ac8d0f5ce410c470432bdb9d544c91f56701c2d
aditi-giri <aditigiri001@gmail.com>
2026-02-08 18:35:36 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: https://writeitdown-1.onrender.com,
 });
 
 export default authApi;
